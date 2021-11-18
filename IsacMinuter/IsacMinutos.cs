bb69b693e7a8abc308c71307d0c3c2aa35e3ddc4
oskar moquist <Oskar.Moquist@gmail.com>
2021-11-18 13:12:05 +0100

    Add project files.

---
+++ IsacMinuter/IsacMinutos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsacMinuter
+{
+    public class IsacMinutos
+    {
+        public double HurMångaMinuterSaHan(double minuter)
+        {
+            double isacMinuter = minuter * 2.25;
+
+            return isacMinuter;
+        }
+    }
+}
---
+++ IsacMinuter/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using IsacMinuter;
+
+Console.WriteLine("Om hur många minuter sa Isac att han var klar?");
+
+IsacMinutos isacMinuter = new IsacMinutos();
+
+string input = Console.ReadLine();
+
+double Isacminuter = Double.Parse(input);
+
+Isacminuter = isacMinuter.HurMångaMinuterSaHan(Isacminuter);
+
+Console.WriteLine(Isacminuter);
+
